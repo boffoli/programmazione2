1d5d3026ccd941fa6cd22a1b9356ca409b5d5281
Mirco Sipone <mircosipone@gmail.com>
2019-06-06 12:27:15 +0200

    ngn

---
+++ 04.7 Inteface/src/InterfaceName.java
@@ -0,0 +1,10 @@
+public interface InterfaceName {
+	int NameOfMethod1(int a);
+	int NameOfMethod2(int n);
+	//………
+	int NameOfMethodN(int a);
+	final static int finlNameVar1 = 1;
+	final static int finlNameVar2 = 2;
+	//………
+	final static int finlNameVarN = 50;
+}
