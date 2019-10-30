91eeb3beb76f8f4a511372a8ef170a5c5073f403
boffoli <boffoli@localhost>
2019-10-30 17:00:59 +0100

    first commit

--- 07.6 overriding vs overloading/src/InstrumentX.java
+++
@@ -1,5 +0,0 @@
-class InstrumentX {
-	public void play(int NoteX) {//<-
-		System.out.println("InstrumentX.play()");
-	}
-}
--- 07.6 overriding vs overloading/src/NoteX.java
+++
@@ -1,4 +0,0 @@
-class NoteX {
-	public static final int
-	MIDDLE_C = 0, C_SHARP = 1, C_FLAT = 2;
-}
